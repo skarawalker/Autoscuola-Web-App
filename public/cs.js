e3955565998024a12744a141e09669b490d87018
Andrea <andrea.costalonga@outlook.com>
2020-01-14 12:44:58 +0100

    Fixed client_search
    FInalmente grazie a Diego non sono più vergine(?)

--- public/cs.js
+++ public/cs.js
@@ -1,8 +1,6 @@
 $(function(){
     // Handle search form
     $("#searchForm").submit(function(e) {
-        console.log('mmamsmsms')
-        console.log($("#searchForm").val())
         const nome = $('input[name=nome]').val()
         const cognome = $('input[name=cognome]').val()
         const dn = $('input[name=dn]').val()
@@ -15,11 +13,11 @@ $(function(){
             $.each(data.rows, function (i, j) {
                 items.push("<tr><td class='column1'>" + j.nome + "</td> " + "<td class='column2'>" + j.cognome + "</td><td class='column3'>" + j.cod_fis + "</td>" + "<td class='column4'>" + j.data_nascita + "</td>" + "<td class='column5'>" + j.data_iscrizione + "</td>" + "<td class='column6'>" + j.telefono + "</td></tr>");
             });
-    
-            $("<table>", {
-                "class": "response",
-                html: items.join("")
-            }).appendTo("section");
+            $("section").empty()
+            const table = $("<table>", {
+                "class": "response"
+            })
+            table.html(items.join('')).appendTo('section')
         });
     });
 })
