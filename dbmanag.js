89bfffcf21029bf1bd6d8779f13cd85973628995
Starkiller13 <andrea.costalonga@outlook.com>
2020-01-31 19:52:48 +0100

    Wabba Lubba Dub Dub ho messo il database online

--- dbmanag.js
+++ dbmanag.js
@@ -1,28 +1,27 @@
 const { Pool } = require('pg');
 // DB user credentials
-DATABASE_URL = 'postgres://starkiller:74hryq@127.0.0.1:5432/autoscuola';
+//DATABASE_URL = 'postgres://starkiller:74hryq@127.0.0.1:5432/autoscuola';
+DATABASE_URL = 'postgres://kjcwgesn:FNwUBZh-r4U841PCweB3c3qx_kh3B5Is@manny.db.elephantsql.com:5432/kjcwgesn'
 
 const pool = new Pool({
-  connectionString: DATABASE_URL
+    connectionString: DATABASE_URL
 });
 
 // a generic query, that executes all queries you send to it
 function query(text) {
-  return new Promise((resolve, reject) => {
-    pool
-      .query(text)
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((err) => {
-        console.error(error)
-        reject(err);
-      });
-  });
+    return new Promise((resolve, reject) => {
+        pool
+            .query(text)
+            .then((res) => {
+                resolve(res);
+            })
+            .catch((err) => {
+                console.error(error)
+                reject(err);
+            });
+    });
 }
 
 module.exports = {
-  query
+    query
 };
-
-
--- server-autoscuola.js
+++ server-autoscuola.js
@@ -6,10 +6,18 @@ const User = require('./User');
 const bodyParser = require("body-parser");
 
 //Database access token
-const config = {
+/*const config = {
     user: 'starkiller',
     database: 'autoscuola',
     password: '7574hryq',
+    port: 5432
+};*/
+
+const config = {
+    host: 'manny.db.elephantsql.com',
+    user: 'kjcwgesn',
+    database: 'kjcwgesn',
+    password: 'FNwUBZh-r4U841PCweB3c3qx_kh3B5Is',
     port: 5432
 };
 
